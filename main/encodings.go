80be7e17971af4513965bdd90b10df5b2ff70069
Sophie Winter <git@phie.me>
2024-10-25 21:00:35 -0700

    Fix float formatting

--- main/encodings.go
+++ main/encodings.go
@@ -216,7 +216,7 @@ var encodings = []encoding{
 	{
 		Name: "f32",
 		EncoderFunc: func(b []byte) (string, int) {
-			return fmt.Sprintf("%12.6f", math.Float32frombits(binary.LittleEndian.Uint32(b))), len(b)
+			return fmt.Sprintf("%.6g", math.Float32frombits(binary.LittleEndian.Uint32(b))), len(b)
 		},
 		Enabled:    false,
 		ByteLength: 4,
@@ -249,7 +249,7 @@ var encodings = []encoding{
 	{
 		Name: "f64",
 		EncoderFunc: func(b []byte) (string, int) {
-			return fmt.Sprintf("%12.6f", math.Float64frombits(binary.LittleEndian.Uint64(b))), len(b)
+			return fmt.Sprintf("%.6g", math.Float64frombits(binary.LittleEndian.Uint64(b))), len(b)
 		},
 		Enabled:    false,
 		ByteLength: 8,
